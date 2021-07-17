17868c3471adf96a7d94ab103387564c2507dca8
Stefan Geyer <git@stefan-geyer.org>
2021-07-17 12:07:31 +0200

    use setup.py in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,17 +1,25 @@
 with import <nixpkgs> {};
+with pkgs.python39Packages;
 let
-  bibot_packages = python-packages: with python-packages; [
+  bibot_package = buildPythonPackage rec {
+    name = "bibot";
+    src = ./.;
+    propagatedBuildInputs = [setuptools pytest pkgs.libsndfile ];
+  };
+  dev_packages = python-packages: with python-packages; [
+    bibot_package
     pytest
+
     selenium
     beautifulsoup4
-
     pudb
     jedi
   ];
-  bibot_python = python39.withPackages bibot_packages;
+  bibot_python = python39.withPackages dev_packages;
 in
-mkShell {
-  nativeBuildInputs = [
-    bibot_python
-  ];
-}
+  mkShell {
+    nativeBuildInputs = [
+      bibot_python
+    ];
+    buildInputs = [bibot_python];
+  }
